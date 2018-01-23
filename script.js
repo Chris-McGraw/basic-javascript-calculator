c65c946411f32c0cdf496fbc3d245ae37d3173a9
Chris McGraw <chris.rmcgraw1@gmail.com>
2018-01-23 17:29:03 -0500

    script-test-update-7
    added functionality to all numbers for addition operation

--- script.js
+++ script.js
@@ -70,67 +70,156 @@ $(document).ready(function(){
       console.log(numberIndex);
       $("#screen-text").append(1);
     }
+
     else if(numberIndex === 1 && equaled === false) {
       nextNumber += "1";
       console.log(numberIndex);
       $("#screen-text").append(1);
     }
-
-    /* $("#screen-text").append(1); */
   });
 
   $("#button-2").on("click", function(){
     checkZero();
-    currentNumber += "2";
-    $("#screen-text").append(2);
+
+    if(numberIndex === 0 && equaled === false){
+      currentNumber += "2";
+      console.log(numberIndex);
+      $("#screen-text").append(2);
+    }
+
+    else if(numberIndex === 1 && equaled === false) {
+      nextNumber += "2";
+      console.log(numberIndex);
+      $("#screen-text").append(2);
+    }
   });
 
   $("#button-3").on("click", function(){
     checkZero();
-    currentNumber += "3";
-    $("#screen-text").append(3);
+
+    if(numberIndex === 0 && equaled === false){
+      currentNumber += "3";
+      console.log(numberIndex);
+      $("#screen-text").append(3);
+    }
+
+    else if(numberIndex === 1 && equaled === false) {
+      nextNumber += "3";
+      console.log(numberIndex);
+      $("#screen-text").append(3);
+    }
   });
 
   $("#button-4").on("click", function(){
     checkZero();
-    currentNumber += "4";
-    $("#screen-text").append(4);
+
+    if(numberIndex === 0 && equaled === false){
+      currentNumber += "4";
+      console.log(numberIndex);
+      $("#screen-text").append(4);
+    }
+
+    else if(numberIndex === 1 && equaled === false) {
+      nextNumber += "4";
+      console.log(numberIndex);
+      $("#screen-text").append(4);
+    }
   });
 
   $("#button-5").on("click", function(){
     checkZero();
-    currentNumber += "5";
-    $("#screen-text").append(5);
+
+    if(numberIndex === 0 && equaled === false){
+      currentNumber += "5";
+      console.log(numberIndex);
+      $("#screen-text").append(5);
+    }
+
+    else if(numberIndex === 1 && equaled === false) {
+      nextNumber += "5";
+      console.log(numberIndex);
+      $("#screen-text").append(5);
+    }
   });
 
   $("#button-6").on("click", function(){
     checkZero();
-    currentNumber += "6";
-    $("#screen-text").append(6);
+
+    if(numberIndex === 0 && equaled === false){
+      currentNumber += "6";
+      console.log(numberIndex);
+      $("#screen-text").append(6);
+    }
+
+    else if(numberIndex === 1 && equaled === false) {
+      nextNumber += "6";
+      console.log(numberIndex);
+      $("#screen-text").append(6);
+    }
   });
 
   $("#button-7").on("click", function(){
     checkZero();
-    currentNumber += "7";
-    $("#screen-text").append(7);
+
+    if(numberIndex === 0 && equaled === false){
+      currentNumber += "7";
+      console.log(numberIndex);
+      $("#screen-text").append(7);
+    }
+
+    else if(numberIndex === 1 && equaled === false) {
+      nextNumber += "7";
+      console.log(numberIndex);
+      $("#screen-text").append(7);
+    }
   });
 
   $("#button-8").on("click", function(){
     checkZero();
-    currentNumber += "8";
-    $("#screen-text").append(8);
+
+    if(numberIndex === 0 && equaled === false){
+      currentNumber += "8";
+      console.log(numberIndex);
+      $("#screen-text").append(8);
+    }
+
+    else if(numberIndex === 1 && equaled === false) {
+      nextNumber += "8";
+      console.log(numberIndex);
+      $("#screen-text").append(8);
+    }
   });
 
   $("#button-9").on("click", function(){
     checkZero();
-    currentNumber += "9";
-    $("#screen-text").append(9);
+
+    if(numberIndex === 0 && equaled === false){
+      currentNumber += "9";
+      console.log(numberIndex);
+      $("#screen-text").append(9);
+    }
+
+    else if(numberIndex === 1 && equaled === false) {
+      nextNumber += "9";
+      console.log(numberIndex);
+      $("#screen-text").append(9);
+    }
   });
 
   $("#button-0").on("click", function(){
     checkZero();
-    currentNumber += "0";
-    $("#screen-text").append(0);
+
+    if(numberIndex === 0 && equaled === false){
+      currentNumber += "0";
+      console.log(numberIndex);
+      $("#screen-text").append(0);
+    }
+
+    else if(numberIndex === 1 && equaled === false) {
+      nextNumber += "0";
+      console.log(numberIndex);
+      $("#screen-text").append(0);
+    }
   });
 
 /* ----- Operation Fuction Executions ----- */
