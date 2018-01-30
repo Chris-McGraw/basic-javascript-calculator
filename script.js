c0604f2cb4422152ea3c26306741990b2494b4b2
Chris McGraw <chris.rmcgraw1@gmail.com>
2018-01-30 13:11:17 -0500

    calculator-script-update-14

--- script.js
+++ script.js
@@ -38,9 +38,6 @@ $(document).ready(function(){
   }
 
   function addCommas(number){
-
-    /* newNumber = ""; */
-
     if(number.includes(".")){
       i = number.indexOf(".");
       decimalSlice = number.slice(i);
@@ -88,7 +85,11 @@ $(document).ready(function(){
       else if(number.length === 11){
         newNumber = number.slice(0,2) + "," + number.slice(2,5) + "," + number.slice(5,8) + "," + number.slice(8);
       }
-      else if(number.length === 10){
+
+      else if(number.length === 10 && number[0] === "-"){
+        newNumber = number.slice(0,4) + "," + number.slice(4,7) + "," + number.slice(7);
+      }
+      else if(number.length === 10 && number[0] !== "-"){
         newNumber = number.slice(0,1) + "," + number.slice(1,4) + "," + number.slice(4,7) + "," + number.slice(7);
       }
 
@@ -98,7 +99,11 @@ $(document).ready(function(){
       else if(number.length === 8){
         newNumber = number.slice(0,2) + "," + number.slice(2,5) + "," + number.slice(5);
       }
-      else if(number.length === 7){
+
+      else if(number.length === 7 && number[0] === "-"){
+        newNumber = number.slice(0,4) + "," + number.slice(4);
+      }
+      else if(number.length === 7 && number[0] !== "-"){
         newNumber = number.slice(0,1) + "," + number.slice(1,4) + "," + number.slice(4);
       }
 
@@ -108,7 +113,11 @@ $(document).ready(function(){
       else if(number.length === 5){
         newNumber = number.slice(0,2) + "," + number.slice(2);
       }
-      else if(number.length === 4){
+
+      else if(number.length === 4 && number[0] === "-"){
+        newNumber = number;
+      }
+      else if(number.length === 4 && number[0] !== "-"){
         newNumber = number.slice(0,1) + "," + number.slice(1);
       }
     }
